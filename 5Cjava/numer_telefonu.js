c0cedb0ddba521678bfdd63b43022bf645c1b5b6
XernoXD <47901137+XernoXD@users.noreply.github.com>
2025-11-12 14:36:59 +0100

    dwdfwe
    fewwefew

--- 5Cjava/numer_telefonu.js
+++ 5Cjava/numer_telefonu.js
@@ -1,10 +1,15 @@
 
 const numer = document.getElementById("numer");
 const pokaz = document.getElementById("pokaz");
+
 pokaz.addEventListener("change", function() {
 if (this.checked) {
+
+
 numer.classList.remove("blur");
 } else {
+
+
 numer.classList.add("blur");
 }
 });
