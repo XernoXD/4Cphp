01e318fa0a0c015463b254e0bae520e3371501ad
XernoXD <47901137+XernoXD@users.noreply.github.com>
2025-11-17 14:36:58 +0100

    fdbgfdb
    bfdbfdbdb

---
+++ witryny/kalendarz.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalendarz</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <h1>Dni, miesiące, lata...</h1>
+    <div id="paragraph"></div>
+    <div id="one"></div><div id="two"><h2>Sprawdź kto ma imieniny</div><div id="three"></div>
+    <footer>Strone opracował(a): 00000000</footer>
+</body>
+</html>
